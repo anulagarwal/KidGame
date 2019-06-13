e1428a6a79426866226f4b468da61499a936a96d
UAD\1804182 <anulagarwal12@gmail.com>
2019-06-13 11:55:08 +0100

    KidGame

---
+++ BK/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool IsPaused;
+
+    public GameObject PausePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TogglePause()
+    {
+
+
+    }
+
+    public void PauseGame()
+    {
+
+
+    }
+
+    public void UnPauseGame()
+    {
+
+
+    }
+}
